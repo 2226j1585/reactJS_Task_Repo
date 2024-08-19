996fdd7ad3d9fdb06619b3a5a79ba5f255a045c0
Visalini Perumal <144419759+2226j1585@users.noreply.github.com>
2024-08-19 21:09:46 +0530

    App.js

---
+++ Task 6/Component/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Task6 } from './components/Task6';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <Task6/>
+      </>
+    </div>
+  );
+}
+
+export default App;
